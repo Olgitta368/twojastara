953c521abb720a47cbdff1e55c23e0167bc5383f
Olgitta368 <omazurek368@technikumkreatywne.pl>
2022-04-13 09:54:20 +0200

    Ciag Fibonacciego

---
+++ ciagfibonacciego.cpp
@@ -0,0 +1,29 @@
+﻿// ciagfibonacciego.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int f0;
+    int f1;
+    int f;
+    cout << "ile kolejnych liczb Fibonacciego chcesz wypisać? \n";
+    cin >> n;
+    f0 = 0;
+    f1 = 1;
+    for (int i = 0; i < n; i++)
+    {
+        if (i > 1) {
+            f = f0 + f1;
+            f0 = f1;
+            f1 = f;
+        }
+        else {
+            f = i;
+        }
+        cout << f << " ";
+    }
+}
+
