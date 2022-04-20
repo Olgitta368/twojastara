5b5e67e74571f5ec69504d9dfe4a358dd87b22f4
Olgitta368 <omazurek368@technikumkreatywne.pl>
2022-04-20 09:51:33 +0200

    liczby pierwsze

---
+++ liczby pierwsze.cpp
@@ -0,0 +1,23 @@
+﻿// liczby pierwsze.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int lp = 0;
+	int p = 2;
+	int n = 15;
+	while (lp < n)
+	{
+		for (int d = 2; d <= p - 1; d++) {
+			if (p % d == 0) {
+				p = p + 1;
+			}
+	}
+		cout << p << " ";
+		lp = lp + 1;
+		p = p + 1;
+	}
+}
+
